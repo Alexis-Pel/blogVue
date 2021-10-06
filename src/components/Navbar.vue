787fd4e49fd7e46303586570db2689ed95ec4fe2
ImMael <legrand.mael2002@gmail.com>
2021-10-06 14:24:50 +0200

    Ajout page About

---
+++ src/components/Navbar.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="nav">
+      <div class="nav-left">
+          <span class="title">BLOG</span>
+      </div>
+      <div class="nav-right">
+        <div class="links">
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
+            <router-link to="/admin">Admin</router-link>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+    .nav{
+        display:flex;
+        height:10%;
+        width: 100%;
+    }
+    .nav-left{
+        display:flex;
+        height:100%;
+        width: 30%;
+        justify-content: center;
+        align-items: center;
+    }
+    .nav-right{
+        display:flex;
+        height:100%;
+        width: 70%;
+        justify-content: flex-end;
+    }
+    .title{
+        display:flex;
+        font-size: 2em;
+    }
+    .links{
+        display:flex;
+        justify-content:space-evenly;
+        align-items: center;
+        height:100%;
+        width: 40%;
+    }
+    .nav a {
+        font-weight: bold;
+        color: #2c3e50;
+
+    }
+
+    .nav a.router-link-exact-active {
+        color: #42b983;
+    }
+</style>
