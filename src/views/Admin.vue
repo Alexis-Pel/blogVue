7cd4bc867180fc5e4457800bb8bbf2d2482fe958
ImMael <legrand.mael2002@gmail.com>
2021-10-07 09:10:29 +0200

    Refonte page Admin (CSS)

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,69 +1,93 @@
 <template>
-  <div class="fond">
-    <div>
-      <table>
-        <tr v-for="(todo, index) in load" :key="todo.id" class="row">
-          <th class="article" :id="todo.title">{{todo.title}}</th>
-          <th class="article" :id="todo.intro">{{todo.intro}}</th>
-          <th class="article" :id="todo.author">{{todo.author}}</th>
-          <button class="edit" @click="edit(index)">editer</button>
-          <button class="edit" @click="suppr(index)">supprimer</button>
-        </tr>
-        <tr>
-          <th><input type="text" name="title" id="title" placeholder="titre"></th>
-          <th><input type="text" name="intro" id="intro" placeholder="introduction"></th>
-          <th><input type="text" name="author" id="author" placeholder="auteur"></th>
-          <th><button>ajouter</button></th>
-        </tr>
+  <section class="container mx-auto p-6 font-mono flex items-center flex-col">
+  <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
+    <div class="w-full overflow-x-auto">
+      <table class="w-full">
+        <thead>
+          <tr class="text-md font-semibold tracking-wide text-left text-gray-100 bg-gray-900 uppercase border-b border-gray-600">
+            <th class="px-4 py-3">Author</th>
+            <th class="px-4 py-3">Title</th>
+            <th class="px-4 py-3">Intro</th>
+            <th class="px-4 py-3">Date</th>
+            <th class="px-4 py-3">Actions</th>
+          </tr>
+        </thead>
+        <tbody class="bg-white">
+          <tr v-for="(todo, index) in load" :key="todo.id" class="text-gray-700">
+            <td class="px-4 py-3 border">
+              <div class="flex items-center text-sm">
+                <div>
+                  <p class="font-semibold text-black">{{todo.author}}</p>
+                </div>
+              </div>
+            </td>
+            <td class="px-4 py-3 text-ms font-semibold border">{{todo.title}}</td>
+            <td class="px-4 py-3 text-xs border">
+              <span class="px-2 py-1 font-semibold leading-tight text-gray-700"> {{todo.intro}} </span>
+            </td>
+            <td class="px-4 py-3 text-sm border">{{todo.date}}</td>
+            <td class="px-4 py-3 text-xs border">
+              <a href="#" class="text-indigo-600 font-bold hover:text-indigo-900 mr-2">Edit</a>
+              <span @click="suppr(index)" class="select-none cursor-pointer text-red-400 font-bold hover:text-red-600">Delete</span>
+            </td>
+          </tr>
+        </tbody>
       </table>
-      <textarea name="content" id="" cols="137" rows="10" placeholder="Écris le contenu de l'article..."></textarea>
     </div>
   </div>
+  <div>
+      <div class="bg-white rounded p-4 px-4 md:p-8 mb-6">
+        <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-2">
+          <div class="lg:col-span-2">
+            <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
+              <div class="md:col-span-2">
+                <label for="author">Author</label>
+                <input type="text" name="author" id="author" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" />
+              </div>
+
+              <div class="md:col-span-3">
+                <label for="title">Title</label>
+                <input type="text" name="title" id="title" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" placeholder="" />
+              </div>
+
+              <div class="md:col-span-2">
+                <label for="intro">Intro</label>
+                <textarea type="text" name="intro" id="intro" class="h-10 border mt-1 rounded px-4 py-2 w-full bg-gray-50" value="" placeholder="" />
+              </div>
+
+              <div class="md:col-span-3">
+                <label for="content">Content</label>
+                <textarea type="text" name="content" id="content" class="h-10 border mt-1 rounded px-4 py-2 w-full bg-gray-50" value="" placeholder="" />
+              </div>
+
+              <div class="md:col-span-5 text-right">
+                <div class="inline-flex items-end">
+                  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2 rounded">Edit</button>
+                  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add</button>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+
 </template>
 
 <script>
 export default {
   name: 'Admin',
-  methods:{
+ computed:{
+     load(){
+         return this.$store.state.articles
+     }
+ },
+ methods:{
     suppr(index){
       this.$store.dispatch('removeTodoByID', index)
     },
-  },
-  computed:{
-    load(){
-      return this.$store.state.articles
-    }
   }
 }
 </script>
-
-<style scoped>
-table{
-  width: 100%;
-  border-width: none;
-}
-th{
-  border: 2px solid black;
-  border-radius: 2.5px;
-}
-button{
-  margin-left: 10%;
-  border: 2px solid black;
-  border-radius: 5px;
-}
-button.edit{
-  margin-left: 10%;
-}
-input{
-  text-align: center;
-  width: 100%;
-  border-width: 0;
-  padding: 0;
-}
-textarea{
-  margin-top: 25px;
-  border: 2px solid black;
-  border-radius: 5px;
-  color: black;
-}
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,10 @@
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
   <!--  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <ShortArticle/>
-    <!-- <admin-test/> -->
   </div>
 </template>
 
 <script>
-// import AdminTest from '../components/AdminTest.vue'
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 import ShortArticle from '../components/ShortArticle.vue'
@@ -16,7 +14,6 @@ import ShortArticle from '../components/ShortArticle.vue'
 export default {
   name: 'Home',
   components: {
-    // AdminTest
     // HelloWorld,
     ShortArticle
   }
